d35ffa2dde27765e986e6405c3199475c6f22b20
VICTOR HUGO COSTA DA SILVA <victor.hcsilva13@senacsp.edu.br>
2022-02-21 11:31:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appmysqlfinal/Hero.java
@@ -0,0 +1,63 @@
+package com.example.appmysqlfinal;
+
+public class Hero {
+    private int id;
+    private String name, realName;
+    private int rating;
+    private String teamaffiliation;
+
+    public Hero(int id, String name, String realName, int rating, String teamaffiliation) {
+        this.id = id;
+        this.name = name;
+        this.realName = realName;
+        this.rating = rating;
+        this.teamaffiliation = teamaffiliation;
+    }
+
+    //CRIANDO OS GETTERS AND SETTERS  alt+insert
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getTeamaffiliation() {
+        return teamaffiliation;
+    }
+
+    public void setTeamaffiliation(String teamaffiliation) {
+        this.teamaffiliation = teamaffiliation;
+    }
+
+
+
+}
+
+
