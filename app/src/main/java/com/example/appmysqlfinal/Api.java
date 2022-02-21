3fec231568ec1974b53c16110becf590a3611e12
victor.hcsilva13 <victor.hccont@gmail.com>
2022-02-21 11:54:03 -0300

    COMENTARIO COMENTADO

---
+++ app/src/main/java/com/example/appmysqlfinal/Api.java
@@ -0,0 +1,14 @@
+package com.example.appmysqlfinal;
+
+public class Api {
+
+    private static final String ROOT_URL = "";
+
+    private static final  String URL_CREATE_HERO = ROOT_URL + 'createhero';
+    private static final  String URL_READ_HEROES = ROOT_URL + 'hetheroes';
+    private static final  String URL_UPDATE_HERO = ROOT_URL + 'updatehero';
+    private static final  String URL_DELETE_HERO = ROOT_URL + 'deletehero&id=';
+
+
+
+}
